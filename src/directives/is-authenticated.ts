5588ec90274c2b919c659553abbf3a6a4e88c5d8
fitsumayalew <fitsumayalew047@gmail.com>
2020-03-28 00:20:45 +0300

    (feat) Implemented is authenticated directive

---
+++ src/directives/is-authenticated.ts
@@ -0,0 +1,31 @@
+import {
+  gql,
+  SchemaDirectiveVisitor,
+  AuthenticationError,
+} from "apollo-server-express";
+import { defaultFieldResolver } from "graphql";
+
+const typeDef = gql`
+  directive @isAuthenticated on FIELD_DEFINITION
+`;
+
+export class IsAuthenticatedDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field: any) {
+    const { resolve = defaultFieldResolver } = field;
+
+    field.resolve = async function (...args: any[]) {
+      const context = args[2];
+
+      if (!context || !context.user) {
+        throw new AuthenticationError("Not allowed");
+      }
+
+      return resolve.apply(this, args);
+    };
+  }
+}
+
+export default {
+  typeDef,
+  directive: IsAuthenticatedDirective,
+};
