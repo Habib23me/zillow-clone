d143956915c0c64c6a7de6fdb83670e557e5bb2e
fitsumayalew <fitsumayalew047@gmail.com>
2020-03-28 00:18:33 +0300

    (feat) Added User Model

---
+++ src/models/user.ts
@@ -0,0 +1,30 @@
+import { Model, Modifiers } from "objection";
+
+export default class User extends Model {
+  id!: number;
+  email!: string;
+  password!: string;
+  firstName?: string;
+  lastName?: string;
+  username!: string;
+  image?: string;
+  role!: number;
+
+  static tableName = "user";
+
+  static jsonSchema = {
+    type: "object",
+    required: ["email", "username", "role"],
+
+    properties: {
+      id: { type: "integer" },
+      email: { type: "string", minLength: 1, maxLength: 255 },
+      password: { type: "string", minLength: 1, maxLength: 255 },
+      first_name: { type: "string", minLength: 1, maxLength: 255 },
+      last_name: { type: "string", minLength: 1, maxLength: 255 },
+      username: { type: "string", minLength: 1, maxLength: 255 },
+      image: { type: "string" },
+      role: { type: "integer" },
+    },
+  };
+}
