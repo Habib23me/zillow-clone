97e46b4c6948ec089c6d9f1a68f3610146acb477
fitsumayalew <fitsumayalew047@gmail.com>
2020-03-31 00:22:00 +0300

    (feat) Added  auhentication

---
+++ src/utils/context.ts
@@ -0,0 +1,30 @@
+import User from "../models/user";
+import config from "../utils/config";
+import jsonwebtoken from "jsonwebtoken";
+import express from "express";
+
+import { AuthenticationError, ApolloServer } from "apollo-server-express";
+
+const getUser = async (req: any) => {
+  const token = req.headers.authorization;
+  if (token) {
+    const { username } = (await jsonwebtoken.verify(
+      token,
+      config.JWT_SECRET
+    )) as { username: string };
+    if (username) {
+      const user = await User.query()
+        .select("id", "username", "role")
+        .findOne("username", "=", username);
+
+      if (user) {
+        return user;
+      }
+    }
+  }
+  return new AuthenticationError("Invalid Authentication Token");
+};
+
+export default {
+  getUser,
+};
