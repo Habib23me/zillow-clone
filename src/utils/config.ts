8e690ac47ed00b050070c57383c0a317fdf0be9d
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-09 13:40:46 +0300

    (fix) changed dot env to be only loaded on devleopment

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,10 +1,11 @@
+if (process.env.NODE_ENV == "development") {
+  require("dotenv").config();
+}
 const PORT = process.env.PORT || 4000;
 const WORKERS = process.env.WORKERS || 1;
 const JWT_LIFE_TIME = process.env.JWT_LIFE_TIME || "15m";
 const CLOUDINARY_FOLDER = process.env.CLOUDINARY_FOLDER || "zillow";
 const DATABASE_URL = process.env.DATABASE_URL || "";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET || "";
 const NODE_ENV = process.env.NODE_ENV || "production";
