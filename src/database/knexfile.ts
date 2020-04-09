a28755102844de47c23420dcf1a1bc9d55e3c154
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-09 14:21:55 +0300

    (fix) using wrong Database URL

--- src/database/knexfile.ts
+++ src/database/knexfile.ts
@@ -1,10 +1,10 @@
 // Update with your config settings.
 
-import DATABASE_URL from "../utils/config";
-
+import config from "../utils/config";
+console.log(DATABASE_URL);
 export = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: {
       database: "zillow",
       user: "postgres",
@@ -20,18 +20,26 @@ export = {
   },
 
   staging: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: "pg",
+    connection: config.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     ssl: true,
   },
   production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: "pg",
+    connection: config.DATABASE_URL,
     migrations: {
       tableName: "knex_migrations",
+    },
+    pool: {
+      min: 2,
+      max: 10,
     },
     ssl: true,
   },
