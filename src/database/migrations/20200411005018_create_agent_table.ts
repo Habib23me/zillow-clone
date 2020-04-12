dc25d5618539b9d9d24e52a565587d011095e66d
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-13 01:50:21 +0300

    (feat) Added agent table migration

---
+++ src/database/migrations/20200411005018_create_agent_table.ts
@@ -0,0 +1,30 @@
+import * as Knex from "knex";
+
+export async function up(knex: Knex): Promise<any> {
+  return knex.schema.createTable("agent", (table) => {
+    table
+      .bigInteger("userId")
+      .unsigned()
+      .index()
+      .references("id")
+      .inTable("user")
+      .primary();
+    table.string("phone");
+    table.string("brokerageName");
+    table.string("brokerageAddress");
+    table.string("city");
+    table.string("zipCode");
+    table.string("licenseNumber");
+    table.string("licenceExpirationDate");
+    table.string("website");
+    table.string("blog");
+    table.string("facebook");
+    table.string("twitter");
+    table.string("linkedId");
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<any> {
+  return knex.schema.dropTable("agent");
+}
