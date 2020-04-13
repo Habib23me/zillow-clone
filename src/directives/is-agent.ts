f679ed66878a8ac1a70811fb705edd506704b7a4
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-13 03:01:34 +0300

    (chor) Added  is agent test

---
+++ src/directives/is-agent.ts
@@ -0,0 +1,31 @@
+import {
+  gql,
+  SchemaDirectiveVisitor,
+  AuthenticationError,
+} from "apollo-server-express";
+import { defaultFieldResolver } from "graphql";
+
+const typeDef = gql`
+  directive @isAgent on FIELD_DEFINITION
+`;
+
+export class IsAgentDirective extends SchemaDirectiveVisitor {
+  visitFieldDefinition(field: any) {
+    const { resolve = defaultFieldResolver } = field;
+
+    field.resolve = async function (...args: any[]) {
+      const context = args[2];
+
+      if (context.user.role != 2) {
+        throw new AuthenticationError("Not allowed");
+      }
+
+      return resolve.apply(this, args);
+    };
+  }
+}
+
+export default {
+  typeDef,
+  directive: IsAgentDirective,
+};
