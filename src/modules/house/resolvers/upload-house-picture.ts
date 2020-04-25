c3a92db7a4618361a99442b481a7e751e1150829
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-25 04:09:16 +0300

    (feat) Added Upload house picture resolver

---
+++ src/modules/house/resolvers/upload-house-picture.ts
@@ -0,0 +1,27 @@
+import User from "../../../models/user";
+import Image from "../../../models/image";
+import uploadImageStream from "../../../utils/upload";
+import { ApolloError } from "apollo-server-express";
+
+const uploadHousePicture = async (
+  parent,
+  { file },
+  { user }: { user: User }
+) => {
+  const { createReadStream, filename, mimetype, encoding } = await file;
+  try {
+    const result = await uploadImageStream(createReadStream(), "/house-images");
+    const image = await Image.query().insert({
+      imagePath: result.public_id,
+    });
+    return {
+      id: image.id,
+      publicId: result.public_id,
+      url: result.se0cure_url,
+    };
+  } catch (error) {
+    throw new ApolloError("Image Upload Failed");
+  }
+};
+
+export default uploadHousePicture;
