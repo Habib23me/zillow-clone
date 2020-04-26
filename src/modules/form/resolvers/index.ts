dbc815abd85514dfea932f124154dd5dd8c6ec1f
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-26 16:50:09 +0300

    (feat) Added Inteface Form

---
+++ src/modules/form/resolvers/index.ts
@@ -0,0 +1,16 @@
+import ContactForm from "../../../models/contact-form";
+import RentForm from "../../../models/rent-form";
+
+const resolvers = {
+  Form: {
+    __resolveType(form: ContactForm | RentForm) {
+      return form.formType();
+    },
+  },
+  FormType: {
+    CONTACT_FORM: "ContactForm",
+    RENT_FORM: "RentForm",
+  },
+};
+
+export default resolvers;
