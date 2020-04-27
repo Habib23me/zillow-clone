60185ed24e0f8eb2290a351a6cf195c355c9ec61
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-27 03:34:10 +0300

    (fix) Fixed when agent not found bug

--- src/modules/agent/resolvers/agent.ts
+++ src/modules/agent/resolvers/agent.ts
@@ -2,7 +2,11 @@ import Agent from "../../../models/agent";
 
 const agent = async (_, args) => {
   //Get agent info by it's id
-  return await Agent.query().findById(args.id);
+  const agent = await Agent.query().findById(args.id);
+  if (!agent) {
+    return Error("Agent not found!");
+  }
+  return agent;
 };
 
 export default agent;
