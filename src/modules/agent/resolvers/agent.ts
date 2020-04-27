272c7ec01612d7b506f2bd7ea40e9d45addbab83
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-27 03:31:48 +0300

    (refactor) commented agent code

--- src/modules/agent/resolvers/agent.ts
+++ src/modules/agent/resolvers/agent.ts
@@ -1,6 +1,7 @@
 import Agent from "../../../models/agent";
 
 const agent = async (_, args) => {
+  //Get agent info by it's id
   return await Agent.query().findById(args.id);
 };
 
--- src/modules/agent/resolvers/agentProfile.ts
+++ src/modules/agent/resolvers/agentProfile.ts
@@ -1,6 +1,6 @@
 import User from "../../../models/user";
-
 const agentProfile = async (_, args, { user }: { user: User }) => {
+  //Get current logged in user's agent profile
   return await user.$relatedQuery("agentProfile");
 };
 
