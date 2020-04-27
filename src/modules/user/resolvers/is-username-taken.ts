bd84c62be61533bd208bf6bbc76609209f3aa73f
fitsumayalew <fitsumayalew047@gmail.com>
2020-04-27 13:01:00 +0300

    (feat) Implemented is usename taken

--- src/modules/user/resolvers/is-username-taken.ts
+++ src/modules/user/resolvers/is-username-taken.ts
@@ -1,6 +1,8 @@
 import User from "../../../models/user";
 
 const isUsernameTaken = async (_, args) => {
+  //check if username is already in use and return a boolean
+
   if (await User.query().findOne(args)) {
     return true;
   }
